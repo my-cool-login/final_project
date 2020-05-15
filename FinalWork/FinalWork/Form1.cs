cfc2d91c05128379a299ce87c646b6922c143659
zukaatoku <zukaatoku@gmail.com>
2020-05-15 16:47:48 +0500

    Добавил кнопку "button1"

--- FinalWork/FinalWork/Form1.cs
+++ FinalWork/FinalWork/Form1.cs
@@ -16,5 +16,10 @@ namespace FinalWork
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button action!");
+        }
     }
 }
